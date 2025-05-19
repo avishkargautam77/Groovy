590bfb617b111b31240495698cb2958af2cc09a3
avishkargautam77 <avishkar@recruitcrm.io>
2025-05-19 10:28:43 +0530

    correcting pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,21 +3,18 @@ pipeline {
     parameters {
         booleanParam(name: 'ENABLE_SCHEDULE', defaultValue: true, description: 'Enable automatic scheduling')
     }
-    
+
     triggers {
         cron(params.ENABLE_SCHEDULE ? 'H/5 * * * *' : '')
     }
 
     stages {
         stage('Execute Playwright Test') {
-                stage('Execute Test') {
-                    steps {
-                        script {
-                            build(job: 'SyntheticMonitoring')
-                        }
-                    }
+            steps {
+                script {
+                    build(job: 'SyntheticMonitoring')
                 }
-        }    
+            }
+        }
     }
-}            
-
+}
